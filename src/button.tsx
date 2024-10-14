dfdbbbcaf1cdd93ac8ec07eb82289d3194e501ec
moverq1337 <moverq1337@gmail.com>
2024-10-14 13:04:28 +0300

    second test

--- src/button.tsx
+++ src/button.tsx
@@ -2,6 +2,7 @@ export default function Button() {
 	return (
 		<div className='flex items-center justify-center pt-5'>
 			<button className='bg-red-500 text-white'>xuy sobaki</button>
+			<button className='bg-red-500 text-white'>xuy sobaki</button>
 		</div>
 	)
 }
